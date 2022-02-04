b4c09d161148bb8c04600b4e1cbc8d67df8e1e47
Prasun117 <johnmukherjee1997@gmail.com>
2022-02-04 20:25:59 +0530

    First commit for config dashboard tab

---
+++ src/Components/ContextApi/CustomizeDashBoardContext.js
@@ -0,0 +1,22 @@
+import React, { createContext, useState } from "react";
+
+export const customizeDashboardContext = createContext();
+
+export const CustomizeDashBoardProvider = (props) => {
+  const initialState = {
+    myDashboard: true,
+    salesTool: true,
+    renewals: true,
+    myCommision: true,
+    myCLients: true,
+    reports: true,
+  };
+  const [dashboardTab, setDashboardTab] = useState(initialState);
+  return (
+    <customizeDashboardContext.Provider
+      value={{ dashboardTab: dashboardTab, setDashboardTab: setDashboardTab }}
+    >
+        {props.children}
+    </customizeDashboardContext.Provider>
+  );
+};
--- src/Components/Dashboard/dashbordTabs.js
+++ src/Components/Dashboard/dashbordTabs.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState,useContext } from "react";
 import { Col, Row, Button, Tabs, Tab } from "react-bootstrap";
 import "./dashboard.css";
 import { library } from "@fortawesome/fontawesome-svg-core";
@@ -6,7 +6,7 @@ import { fab } from "@fortawesome/free-brands-svg-icons";
 import SalesTools from "../SalesTools/salesTools";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import * as Icons from "react-bootstrap-icons";
-
+import { customizeDashboardContext } from "../ContextApi/CustomizeDashBoardContext";
 import {
   faCheckSquare,
   faCoffee,
@@ -18,6 +18,8 @@ import Popup from "../Common/Popup";
 library.add(fab, faCheckSquare, faCoffee, faEnvelope, faPhoneAlt);
 function DashboardTabs() {
   const [modalCustomize, setModalCustomize] = useState(false);
+  const dashboardCustomizeContext = useContext(customizeDashboardContext);
+  const customizeDashboard = dashboardCustomizeContext.dashboardTab;
   return (
     <div>
       {/* Customize Dashboard Button Click to open popup */}
@@ -34,37 +36,85 @@ function DashboardTabs() {
         id="noanim-tab-example"
         className="mb-3"
       >
-        <Tab eventKey="My_Dashboard" title={
-        <><Icons.PieChartFill color="text-primary" className="me-1" size={20}/> My Dashboard</>
-       }>
-          <Row className="margin-0">
-            <Col>
-               <i className="tabs-icon"></i>
-              <MyDashboard />
-            </Col>
-          </Row>
-        </Tab>
-        <Tab eventKey="sales" title={
-        <><Icons.BriefcaseFill className="me-1" size={20}/> Sales Tool</>
-       }>
-          <Row className="margin-0">
-            <Col>
-              <SalesTools />
-            </Col>
-          </Row>
-        </Tab>
-        <Tab eventKey="renewals" title={
-        <><Icons.UmbrellaFill className="me-1" size={20}/> Renewals</>
-       }></Tab>
-        <Tab eventKey="commission" title={
-        <><Icons.CurrencyDollar className="me-1" size={20}/> My Commission</>
-       }></Tab>
-        <Tab eventKey="client" title={
-        <><Icons.StarFill className="me-1" size={20}/> My Clients</>
-       }></Tab>
-        <Tab eventKey="reports" title={
-        <><Icons.FileEarmarkTextFill className="me-1" size={20}/> Reports</>
-       }></Tab>
+        {customizeDashboard.myDashboard && (
+          <Tab
+            eventKey="My_Dashboard"
+            title={
+              <>
+                <Icons.PieChartFill
+                  color="text-primary"
+                  className="me-1"
+                  size={20}
+                />{" "}
+                My Dashboard
+              </>
+            }
+          >
+            <Row className="margin-0">
+              <Col>
+                <i className="tabs-icon"></i>
+                <MyDashboard />
+              </Col>
+            </Row>
+          </Tab>
+        )}
+        {customizeDashboard.salesTool && (
+          <Tab
+            eventKey="sales"
+            title={
+              <>
+                <Icons.BriefcaseFill className="me-1" size={20} /> Sales Tool
+              </>
+            }
+          >
+            <Row className="margin-0">
+              <Col>
+                <SalesTools />
+              </Col>
+            </Row>
+          </Tab>
+        )}
+        {customizeDashboard.renewals && (
+          <Tab
+            eventKey="renewals"
+            title={
+              <>
+                <Icons.UmbrellaFill className="me-1" size={20} /> Renewals
+              </>
+            }
+          ></Tab>
+        )}
+        {customizeDashboard.myCommision && (
+          <Tab
+            eventKey="commission"
+            title={
+              <>
+                <Icons.CurrencyDollar className="me-1" size={20} /> My
+                Commission
+              </>
+            }
+          ></Tab>
+        )}
+        {customizeDashboard.myCLients && (
+          <Tab
+            eventKey="client"
+            title={
+              <>
+                <Icons.StarFill className="me-1" size={20} /> My Clients
+              </>
+            }
+          ></Tab>
+        )}
+        {customizeDashboard.reports && (
+          <Tab
+            eventKey="reports"
+            title={
+              <>
+                <Icons.FileEarmarkTextFill className="me-1" size={20} /> Reports
+              </>
+            }
+          ></Tab>
+        )}
       </Tabs>
       {/* Customize Dashboard Popup Component */}
       <Popup
